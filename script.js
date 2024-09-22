227da3e83c14e7873f7b64c2098073efa3f41c1a
Jesika G <160136873+121013jesi@users.noreply.github.com>
2024-09-22 21:10:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,17 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
 
-
-function calculateBMI() {
-    var heightInput = document.getElementById("height");
-    var weightInput = document.getElementById("weight");
-    var resultDiv = document.getElementById("result");
-  
-    var height = parseFloat(heightInput.value);
-    var weight = parseFloat(weightInput.value);
-  
-    if (isNaN(height) || isNaN(weight)) {
-      resultDiv.innerHTML = "Please enter valid height and weight.";
-      return;
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
     }
-  
-    var bmi = weight / ((height / 100) ** 2);
-    var category = "";
-  
-    if (bmi < 18.5) {
-      category = "Underweight";
-    } else if (bmi < 25) {
-      category = "Normal weight";
-    } else if (bmi < 30) {
-      category = "Overweight";
-    } else {
-      category = "Obese";
+    else {
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
     }
-  
-    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
-  }
-
-  
-  function calculateBMI() {
-    var heightInput = document.getElementById("height");
-    var weightInput = document.getElementById("weight");
-    var resultDiv = document.getElementById("result");
-  
-    var height = parseFloat(heightInput.value);
-    var weight = parseFloat(weightInput.value);
-  
-    if (isNaN(height) || isNaN(weight)) {
-      resultDiv.innerHTML = "Please enter valid height and weight.";
-      return;
-    }
-  
-    var bmi = weight / ((height / 100) ** 2);
-    var category = "";
-  
-    if (bmi < 18.5) {
-      category = "Underweight";
-    } else if (bmi < 25) {
-      category = "Normal weight";
-    } else if (bmi < 30) {
-      category = "Overweight";
-    } else {
-      category = "Obese";
-    }
-  
-    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
-  }
+});
